95823eba52590d8f6ef54abee9cde1c8e45cd528
Serg <freekey@list.ru>
2024-03-24 20:18:08 +0400

    jpa-repository-query

--- src/main/java/ru/sergjava/daohibernate/repository/PersonRepositoryJPA.java
+++ src/main/java/ru/sergjava/daohibernate/repository/PersonRepositoryJPA.java
@@ -2,6 +2,8 @@ package ru.sergjava.daohibernate.repository;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.sergjava.daohibernate.model.Person;
 import ru.sergjava.daohibernate.model.PersonID;
@@ -11,9 +13,11 @@ import java.util.Optional;
 
 @Repository
 public interface PersonRepositoryJPA extends JpaRepository<Person, PersonID> {
-    List<Person> findByCityOfLiving(String cityOfLiving);
+    @Query(value = "select p from Person p where p.cityOfLiving=:city")
+    List<Person> findByCityOfLiving(@Param("city") String cityOfLiving);
+    @Query(value = "select p from Person p where p.personID.age<:age")
+    List<Person> findByPersonIDAgeLessThan(@Param("age") Integer age, Sort sort);
 
-    List<Person> findByPersonIDAgeLessThan(Integer age, Sort sort);
-
-    List<Optional<Person>> findByPersonIDNameAndPersonIDSurname(String name, String surname);
+    @Query(value = "select p from Person p where p.personID.name=:name and p.personID.surname=:surname")
+    List<Optional<Person>> findByPersonIDNameAndPersonIDSurname(@Param("name") String name, @Param("surname") String surname);
 }
